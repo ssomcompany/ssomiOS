6e18f92ffb4b34ad8db944fa42e3642af14ef991
DongSoo Lee <jegumhon@gmail.com>
2015-11-02 00:00:29 +0900

    Initialize the default library

---
+++ Ssom/Ssom/3rdLib/IOS_DaumMap_SDK_1.2.16/doc/search/properties_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['offsetx',['offsetX',['../struct_m_t_map_image_offset.html#a7828eddac89a55058f0e1c003cf8362b',1,'MTMapImageOffset']]]
+];
---
+++ Ssom/Ssom/3rdLib/IOS_DaumMap_SDK_1.2.16/doc/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['offsety',['offsetY',['../struct_m_t_map_image_offset.html#a3eaf907ce8ccbbf3044669a83c1410a9',1,'MTMapImageOffset']]]
+];

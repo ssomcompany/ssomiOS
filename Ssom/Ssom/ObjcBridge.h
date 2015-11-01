9ef93af7069a3347f9003e03481ff913648d026e
DongSoo Lee <jegumhon@gmail.com>
2015-11-02 00:32:43 +0900

    add the Bridging file for using the Objc library

---
+++ Ssom/Ssom/ObjcBridge.h
@@ -0,0 +1,10 @@
+//
+//  ObjcBridge.h
+//  Ssom
+//
+//  Created by DongSoo Lee on 2015. 11. 2..
+//  Copyright © 2015년 SsomCompany. All rights reserved.
+//
+
+#import "AFNetworking/AFNetworking.h"
+#import <DaumMap/MTMapView.h>

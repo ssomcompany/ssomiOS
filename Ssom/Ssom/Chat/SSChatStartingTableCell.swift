8622135e3470ec6f59b665c3a1f0be7ce72f4006
DongSoo Lee <jegumhon@gmail.com>
2016-10-26 00:46:42 +0900

    채팅방 내 시스템 메시지 처리

--- Ssom/Ssom/Chat/SSChatStartingTableCell.swift
+++ Ssom/Ssom/Chat/SSChatStartingTableCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SSChatStartingTableCell: UITableViewCell {
     @IBOutlet var lbColored: UILabel!
+    @IBOutlet var constLbColoredCenterXToSuper: NSLayoutConstraint!
     @IBOutlet var lbDetailMessage: UILabel!
 
     override func awakeFromNib() {
@@ -24,16 +25,22 @@ class SSChatStartingTableCell: UITableViewCell {
             self.lbColored.textColor = UIColor(red: 237.0/255.0, green: 52.0/255.0, blue: 75.0/255.0, alpha: 1)
         }
 
+        self.lbColored.text = "두근두근, "
+        self.constLbColoredCenterXToSuper.constant = -48.0
+        self.lbDetailMessage.text = "대화가 시작 되었쏨 : )"
+
         if let message = model where message.messageType == .System {
             if message.message == "out" {
                 self.lbColored.text = "쏨이 끝났어요, "
-                self.lbDetailMessage.text = "실망하지 말고 다른 상대를 찾아보아요∙∙∙(T_T)"
+                self.lbDetailMessage.text = "다른 상대를∙∙∙(T_T)"
             } else if message.message == "request" {
                 self.lbColored.text = "만남 요청을 받았습니다!"
                 self.lbDetailMessage.text = ""
+                self.constLbColoredCenterXToSuper.constant = 0.0
             } else if message.message == "approve" {
                 self.lbColored.text = "쏨과 만나는 중..."
                 self.lbDetailMessage.text = ""
+                self.constLbColoredCenterXToSuper.constant = 0.0
             }
         }
     }
